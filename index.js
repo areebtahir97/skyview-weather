d8ee033aec22ae14207f8ff9be426760728f5e95
Areeb Tahir <areebtahir97@gmail.com>
2024-03-09 15:58:04 +0530

    It

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 
-app.get("/", (req, res) => {
+app.use("/", (req, res) => {
   res.render("index.ejs", { weather: null, error: null });
   
 });
